da8b560d6113c5f374cbcfeb279770e059f22a5d
tryplochyton <etdyv@yahoo.fr>
2017-01-16 14:54:29 +0100

    Add files via upload

---
+++ harder/SpeakGoodBye.js
@@ -0,0 +1,15 @@
+(function (window) {
+var byeSpeaker: {"name"};
+var name = ["Yaakov", "Paul", "Frank", "Larry", "Paula", "Laura"];
+var speakWord = "Good Bye";
+for (var i = 0; i < name.length; i++) {
+	var [i]Greeter = {};
+  [i]Greeter.name = byeSpeaker;
+  [i]Greeter.sayGoodBye = function speak(name) {
+	console.log(speakWord + [i]Greeter.name);
+}
+};
+
+window.[i]Greeter = byeSpeaker;
+
+})(window);
---
+++ harder/SpeakHello.js
@@ -0,0 +1,14 @@
+(function (window) {
+	var helloSpeaker: {"name"};
+	var name = ["John", "Jen", "Jason", "Jim"];
+	var speakWord = "Hello";
+	for (var i = 0; i < name.length; i++) {
+	var [i]Greeter = {};
+  [i]Greeter.name = helloSpeaker;
+  [i]Greeter.sayHello = function speak(name) {
+	console.log(speakWord + [i]Greeter.name);
+}
+};
+
+window.[i]Greeter = helloSpeaker;
+})(window);
